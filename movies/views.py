b0ffe079a08148eb1388526fae0e7002c3602037
gabaeliz <elizabethg.eg12@gmail.com>
2023-04-25 13:29:15 -0600

    Creacion de los metodos

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .pagination import CustomPagination
 
-# Create your views here.
+
+
+from .models import Actor, Film, Rental, Payment, Inventory
+
+from .serializers import ActorSerializer, FilmSerializer, RentalSerializer, PaymentSerializer, InventorySerializer
+
+class ActorList(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+class FilmList(generics.ListCreateAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+    pagination_class = CustomPagination    
+
+
+class FilmDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
+
+class RentalList(generics.ListCreateAPIView):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer
+    pagination_class = CustomPagination
+
+class RentalDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer
+
+class PaymentList(generics.ListCreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    pagination_class = CustomPagination
+
+
+class PaymentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class InventoryList(generics.ListCreateAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+    #pagination_class = CustomPagination
+
+class InventoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+
+    def get_queryset(self):
+        queryset = Inventory.objects.all()
+        film_id = self.request.query_params.get('film_id', None)
+        if film_id is not None:
+            queryset = queryset.filter(film_id=film_id)
+        return queryset
+
+
+
+
+
+
+
